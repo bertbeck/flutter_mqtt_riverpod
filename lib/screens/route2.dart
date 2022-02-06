e9f06df13bde64d8d2ff19e706122a2b694c3084
Randal L. Schwartz <merlyn@stonehenge.com>
2022-02-06 09:38:34 -0800

    add wifiListProvider

--- lib/screens/route2.dart
+++ lib/screens/route2.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+final piProvider = Provider((ref) => 3.14);
+final wifiListProvider = FutureProvider((ref) async {
+  return Future.delayed(const Duration(seconds: 5), () => ['wifi1', 'wifi2']);
+});
+
 class Route2 extends HookConsumerWidget {
   const Route2({
     Key? key,
@@ -25,14 +30,15 @@ class Route2Page extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final wifiList = ref.watch(wifiListProvider);
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              '[pi connected]',
+            Text(
+              'Pi says $wifiList',
               textAlign: TextAlign.center,
             ),
             ElevatedButton(
