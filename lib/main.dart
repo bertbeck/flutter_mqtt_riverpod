d59d7226589b8c1f75e2ebd9d27a20bf7af0acd0
Randal L. Schwartz <merlyn@stonehenge.com>
2022-02-10 14:34:05 -0800

    integrate amplify settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
-// remember to add flutter_hooks and hooks_riverpod to pubspec.yaml
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'amplifyconfiguration.dart';
 import 'screens/home.dart';
 
-void main() {
+Future<void> main() async {
+  runApp(const Center(child: CircularProgressIndicator()));
+  try {
+    await Amplify.addPlugin(AmplifyAuthCognito());
+    await Amplify.configure(amplifyconfig);
+  } on Exception catch (e) {
+    if (kDebugMode) {
+      print('Could not configure Amplify: $e');
+    }
+  }
   runApp(
     const ProviderScope(
       child: Home(),
