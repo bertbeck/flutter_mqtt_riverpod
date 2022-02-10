c55b295e81b1c455177a1571ef8235f96a01a16e
Randal L. Schwartz <merlyn@stonehenge.com>
2022-02-10 13:17:25 -0800

    amplify pull on sample app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "sampleapp": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3sampleappstoraged2sdrcvvhw2cq4": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
