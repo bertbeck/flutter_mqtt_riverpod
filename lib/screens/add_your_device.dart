68c3d753338fd8350faa53e4e0820cc5b4794944
Randal L. Schwartz <merlyn@stonehenge.com>
2022-02-17 12:57:58 -0800

    create my page frame

--- lib/screens/add_your_device.dart
+++ lib/screens/add_your_device.dart
@@ -9,6 +9,65 @@ class AddYourDevice extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    return MyPageFrame(
+      children: [
+        Text('Add Your Device', style: Theme.of(context).textTheme.headline5),
+        const SizedBox(height: 20),
+        const Text(
+          '''
+1. Connect your Device to Ethernet (optional).
+2. Plug your Device into Power.
+3. Wait 3 Minutes.
+4. Is the LED Red or Blue?
+''',
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+            child: const Text('Red'),
+            onPressed: () async {
+              context.goNamed('connect to pi');
+            }),
+        ElevatedButton(child: const Text('Blue'), onPressed: () {}),
+      ],
+    );
+  }
+}
+
+class MyPageFrame extends HookConsumerWidget {
+  const MyPageFrame({
+    Key? key,
+    required this.children,
+  }) : super(key: key);
+  final List<Widget> children;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: children,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/* class AddYourDeviceOLD extends HookConsumerWidget {
+  const AddYourDeviceOLD({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: SafeArea(
@@ -47,3 +106,4 @@ class AddYourDevice extends HookConsumerWidget {
     );
   }
 }
+*/
