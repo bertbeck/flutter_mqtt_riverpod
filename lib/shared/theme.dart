978f7335c15d0b796e24f2193cdb9ed925c3e1ab
Randal L. Schwartz <merlyn@stonehenge.com>
2022-02-18 11:09:53 -0800

    add the Elevated Button default style (stadium)

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 
 ThemeData getGlobalTheme() {
   return ThemeData.from(
-      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF3aac59)));
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF3aac59)))
+      .copyWith(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(shape: const StadiumBorder())));
 }
