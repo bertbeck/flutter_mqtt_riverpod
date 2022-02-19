a047db8cfd73d41575d917e1d7adacfc8de707b8
Randal L. Schwartz <merlyn@stonehenge.com>
2022-02-19 09:44:29 -0800

    add to dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../api/iot.dart';
 import '../shared/my_page_frame.dart';
+
+extension JsonfromString on String {
+  Map<String, dynamic> toJson() => json.decode(this);
+}
 
 class Dashboard extends HookConsumerWidget {
   const Dashboard({
@@ -11,16 +17,26 @@ class Dashboard extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final getIp = ref.watch(getThingShadowProvider('RandalPi:ip'));
-    final getFarmConfig =
-        ref.watch(getThingShadowProvider('RandalPi:farm_config'));
+    final getIp = ref
+        .watch(getThingShadowProvider('RandalPi:ip'))
+        .value
+        ?.toJson()['state']['reported'];
+    final getFarmConfig = ref
+        .watch(getThingShadowProvider('RandalPi:farm_config'))
+        .value
+        ?.toJson()['state']['reported'];
+    final getFarmStatus = ref
+        .watch(getThingShadowProvider('RandalPi:farm_status'))
+        .value
+        ?.toJson()['state']['reported'];
+
     return MyPageFrame(
       children: [
         Text('Dashboard', style: Theme.of(context).textTheme.headline5),
         const SizedBox(height: 20),
         Text('IP: ${getIp.toString()}'),
         Text('Farm Config: ${getFarmConfig.toString()}'),
-        const Text('Farm status'),
+        Text('Farm Status: ${getFarmStatus.toString()}'),
         const Text('Wallet status'),
         const Text('Device configuration'),
         const Text('Device status'),
